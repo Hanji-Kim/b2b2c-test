997931884f7e7769e969639ad6aa93dc22d8cb9f
“HANJI” <agnes@bizinsight.co.kr>
2025-07-17 15:02:04 +0900

    feat/style: 세팅 및 스타일 추가

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hero } from './components/ui/animated-hero';
+
+function App() {
+  return (
+    <div className="w-screen min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Hero />
+    </div>
+  );
+}
+
+export default App; 
