ef0a1f9985be725f8e2d6f41b53ef15f3bfe4a3d
“HANJI” <agnes@bizinsight.co.kr>
2025-07-17 15:25:21 +0900

    style : ui 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hero } from './components/ui/animated-hero';
+import { Feature } from './components/ui/feature-section-with-grid';
 
 function App() {
   return (
     <div className="w-screen min-h-screen bg-gray-50 dark:bg-gray-900">
       <Hero />
+      <Feature />
     </div>
   );
 }
